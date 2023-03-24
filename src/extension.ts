49247cb4f3080f6006d0ae04a61b3de3a0ee9017
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-03-24 11:25:51 -0300

    Add prettier and config integration with eslint

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,24 @@
 const { spawn } = require("child_process");
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-		if(vscode.workspace.workspaceFolders) {
-			const shortstat = spawn('git', ["diff", "HEAD", "HEAD~1", "--shortstat"], {cwd: vscode.workspace.workspaceFolders[0].uri.path});
-			
-			shortstat.stdout.on('data', (data: any) => {
-				console.log(`stdout: ${data}`);
-			});
+  if (vscode.workspace.workspaceFolders) {
+    const shortstat = spawn("git", ["diff", "HEAD", "HEAD~1", "--shortstat"], {
+      cwd: vscode.workspace.workspaceFolders[0].uri.path,
+    });
 
-			shortstat.stderr.on('data', (data: any) => {
-				console.error(`stderr: ${data}`);
-			});
+    shortstat.stdout.on("data", (data: any) => {
+      console.log(`stdout: ${data}`);
+    });
 
-			shortstat.on('close', (code: any) => {
-				console.log(`child process exited with code ${code}`);
-			});
-		}
+    shortstat.stderr.on("data", (data: any) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    shortstat.on("close", (code: any) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }
 }
 
 export function deactivate() {}
