5840dfff802c9d9a5e0b461cc339e6fc4114a71c
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-03-24 11:12:14 -0300

    Make extension active when vscode startup is finished

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,6 @@ const { spawn } = require("child_process");
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('changed-lines-count.helloWorld', async () => {
 		if(vscode.workspace.workspaceFolders) {
 			const shortstat = spawn('git', ["diff", "HEAD", "HEAD~1", "--shortstat"], {cwd: vscode.workspace.workspaceFolders[0].uri.path});
 			
@@ -22,10 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 				console.log(`child process exited with code ${code}`);
 			});
 		}
-
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
