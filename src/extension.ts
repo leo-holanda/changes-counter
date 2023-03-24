83dafcf777d8f56d7f3c8c9290d78734925b02c9
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-03-24 13:42:44 -0300

    Add changes quantity bar item

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,25 @@ const { spawn } = require("child_process");
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  const changesQuantityBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    10
+  );
+  changesQuantityBarItem.text = "Loading...";
+  changesQuantityBarItem.show();
+  context.subscriptions.push(changesQuantityBarItem);
+
   if (vscode.workspace.workspaceFolders) {
     const shortstat = spawn("git", ["diff", "HEAD", "HEAD~1", "--shortstat"], {
       cwd: vscode.workspace.workspaceFolders[0].uri.path,
     });
 
     shortstat.stdout.on("data", (data: any) => {
-      console.log(`stdout: ${data}`);
+      const splittedDiffOutput = data.toString().split(", ");
+      const insertions = +splittedDiffOutput[1][0];
+      const deletions = +splittedDiffOutput[2][0];
+      const changesQuantity = insertions + deletions;
+      changesQuantityBarItem.text = "Changes: " + changesQuantity.toString();
     });
 
     shortstat.stderr.on("data", (data: any) => {
