3209be43d1ef961658118405e43891e88b3dcdec
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-03-24 10:49:48 -0300

    Add initial git child process

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+const { spawn } = require("child_process");
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "changed-lines-count" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('changed-lines-count.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
+	let disposable = vscode.commands.registerCommand('changed-lines-count.helloWorld', async () => {
+		if(vscode.workspace.workspaceFolders) {
+			const shortstat = spawn('git', ["diff", "HEAD", "HEAD~1", "--shortstat"], {cwd: vscode.workspace.workspaceFolders[0].uri.path});
+			
+			shortstat.stdout.on('data', (data: any) => {
+				console.log(`stdout: ${data}`);
+			});
+
+			shortstat.stderr.on('data', (data: any) => {
+				console.error(`stderr: ${data}`);
+			});
+
+			shortstat.on('close', (code: any) => {
+				console.log(`child process exited with code ${code}`);
+			});
+		}
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
