134f9f651d50bdb4f9ae5a11f7010ca3ac341d68
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-04-13 18:06:31 -0300

    Add initial unit test

---
+++ src/test/unit-tests/unit.test.ts
@@ -0,0 +1,8 @@
+import assert = require("assert");
+
+describe("should work", () => {
+  it("should work", () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
+});
