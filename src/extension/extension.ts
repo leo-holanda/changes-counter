8adbba5cf4558a50c4f658d6d5c7bdb2dac9765d
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-11-06 10:56:03 -0300

    Add notificator to extension class

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,12 +1,15 @@
 import { GitOperator } from "../gitOperator/gitOperator";
 import { Logger } from "../logger/logger";
+import { Notificator } from "../notificator/notificator";
 
 export class Extension {
   logger!: Logger;
   gitOperator!: GitOperator;
+  notificator!: Notificator;
 
   constructor() {
     this.logger = new Logger();
     this.gitOperator = new GitOperator();
+    this.notificator = new Notificator();
   }
 }
