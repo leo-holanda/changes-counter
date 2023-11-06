ac866441f77f6a46a74e85c2314c83dc837827cf
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-11-06 10:28:36 -0300

    Add git operator in extension class

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,9 +1,12 @@
+import { GitOperator } from "../gitOperator/gitOperator";
 import { Logger } from "../logger/logger";
 
 export class Extension {
   logger!: Logger;
+  gitOperator!: GitOperator;
 
   constructor() {
     this.logger = new Logger();
+    this.gitOperator = new GitOperator();
   }
 }
