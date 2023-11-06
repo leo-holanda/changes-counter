363633ed7867d99b18a5b2d49ed97bc32a3f1554
Leonardo Holanda <leo.uldholanda@gmail.com>
2023-11-06 14:06:46 -0300

    Use logger as singleton in extension class

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,19 +1,21 @@
 import { BarItem } from "../BarItem/BarItem";
 import { GitOperator } from "../gitOperator/gitOperator";
 import { Logger } from "../logger/logger";
+import { LogTypes } from "../logger/logger.enums";
 import { Notificator } from "../notificator/notificator";
 import * as vscode from "vscode";
 
 export class Extension {
-  logger!: Logger;
   gitOperator!: GitOperator;
   notificator!: Notificator;
   barItem!: BarItem;
+  logger: Logger;
 
   constructor(context: vscode.ExtensionContext) {
-    this.logger = new Logger();
+    this.logger = Logger.getInstance();
     this.gitOperator = new GitOperator();
     this.notificator = new Notificator();
     this.barItem = new BarItem(context);
+    this.logger.log("Extension was constructed.", LogTypes.INFO);
   }
 }
